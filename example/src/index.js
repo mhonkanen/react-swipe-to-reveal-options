0dc9cdc9a2d689dbe0065ea975b052fb6765068c
Charles Berman <Charles.Berman@Magna.com>
2018-09-25 08:42:59 -0400

    test build

--- example/src/index.js
+++ example/src/index.js
@@ -1,53 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import './css/style.css';
 import './css/swipe-to-reveal-options.css';
 import { SwipeToRevealList } from './components';
 
 const leftOptions = [{ label: 'Undo', class: 'undo' }];
 const rightOptions = [{ label: 'Complete', class: 'complete' }];
-var items = [
+
+const items = [
 	{
-		leftOptions: leftOptions,
-		rightOptions: rightOptions,
+		leftOptions,
+		rightOptions,
 		content: 'Mail from Mathieu',
 		callActionWhenSwipingFarLeft: true,
 		callActionWhenSwipingFarRight: true
 	},
 	{
-		leftOptions: leftOptions,
-		rightOptions: rightOptions,
+		leftOptions,
+		rightOptions,
 		content: 'Mail from Arseny',
 		callActionWhenSwipingFarRight: true,
 		callActionWhenSwipingFarLeft: false
 	},
 	{
-		leftOptions: leftOptions,
-		rightOptions: rightOptions,
+		leftOptions,
+		rightOptions,
 		content: 'Mail from Bruno',
 		callActionWhenSwipingFarRight: false,
 		callActionWhenSwipingFarLeft: false
 	}
 ];
-
-const item = {
-	leftOptions: [
-		{
-			label: 'Undo',
-			class: 'undo'
-		}
-	],
-	rightOptions: [
-		{
-			label: 'Complete',
-			class: 'complete'
-		}
-	],
-	content: `item `,
-	callActionWhenSwipingFarLeft: true,
-	callActionWhenSwipingFarRight: true
-};
 
 const App = () => <SwipeToRevealList items={items} />;
 
