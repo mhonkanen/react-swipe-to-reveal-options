a8df0312e0bd2bf455e8becc8fc35505f8451fa5
Charles Berman <Charles.Berman@Magna.com>
2019-09-09 10:49:42 -0400

    clean and publish

---
+++ src/__tests__/test.js
@@ -0,0 +1,34 @@
+import LeftRightButton from '../LeftRightButton';
+import Swipeable from '../Swipeable';
+import SwipeItem from '../SwipeItem';
+import SwipeToRevealList from '../SwipeToRevealList';
+import SwipeToRevealOptions from '../SwipeToRevealOptions';
+
+describe('LeftRightButton', () => {
+	it('is truthy', () => {
+		expect(LeftRightButton).toBeTruthy();
+	});
+});
+
+describe('Swipeable', () => {
+	it('is truthy', () => {
+		expect(Swipeable).toBeTruthy();
+	});
+});
+
+describe('SwipeItem', () => {
+	it('is truthy', () => {
+		expect(SwipeItem).toBeTruthy();
+	});
+});
+
+describe('SwipeToRevealOptions', () => {
+	it('is truthy', () => {
+		expect(SwipeToRevealOptions).toBeTruthy();
+	});
+});
+describe('SwipeToRevealList', () => {
+	it('is truthy', () => {
+		expect(SwipeToRevealList).toBeTruthy();
+	});
+});
--- src/components/index.jsx
+++
@@ -1,4 +0,0 @@
-export {default as LeftRightButton} from './LeftRightButton';
-export {default as Swipeable} from './Swipeable';
-export {default as SwipeToRevealList} from './SwipeToRevealList';
-export {default as SwipeToRevealOptions} from './SwipeToRevealOptions';
