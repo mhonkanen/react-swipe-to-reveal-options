25e71b2adabb068a6c2442c23195172d672f8540
Charles Berman <Charles.Berman@magna.com>
2019-09-09 20:13:47 -0400

    updating documentation

--- src/SwipeToRevealList.jsx
+++ src/SwipeToRevealList.jsx
@@ -1,45 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import SwipeToRevealOptions from './SwipeToRevealOptions.jsx'
+import { SwipeItemPropType } from './Props';
+
 import './index.css'
 const SwipeToRevealList = ({ items, width }) => (
-  <div>
-    {items.map((item, index) => {
+	<div>
+		{items.map((item, index) => {
 			const key = `options-key-${index}`
 			return (
-  <SwipeToRevealOptions
-    key={key}
-    leftOptions={item.leftOptions}
-    rightOptions={item.rightOptions}
-    callActionWhenSwipingFarRight={item.callActionWhenSwipingFarRight}
-    callActionWhenSwipingFarLeft={item.callActionWhenSwipingFarLeft}
-    actionThreshold={width}
+				<SwipeToRevealOptions
+					key={key}
+					leftOptions={item.leftOptions}
+					rightOptions={item.rightOptions}
+					callActionWhenSwipingFarRight={item.callActionWhenSwipingFarRight}
+					callActionWhenSwipingFarLeft={item.callActionWhenSwipingFarLeft}
+					actionThreshold={width}
 				>
-    {item.content}
-  </SwipeToRevealOptions>
+					{item.content}
+				</SwipeToRevealOptions>
 			)
 		})}
-  </div>
+	</div>
 )
 
-const OptionType = PropTypes.shape({
-	label: PropTypes.string.isRequired,
-	class: PropTypes.string.isRequired
-})
 SwipeToRevealList.defaultProps = {
 	width: window.innerWidth
 }
 SwipeToRevealList.propTypes = {
 	width: PropTypes.number.isRequired,
-	items: PropTypes.arrayOf(
-		PropTypes.shape({
-			leftOptions: PropTypes.arrayOf(OptionType),
-			rightOptions: PropTypes.arrayOf(OptionType),
-			content: PropTypes.any,
-			callActionWhenSwipingFarLeft: PropTypes.bool,
-			callActionWhenSwipingFarRight: PropTypes.bool
-		})
-	)
+	items: PropTypes.arrayOf(SwipeItemPropType)
 }
 
 export default SwipeToRevealList
